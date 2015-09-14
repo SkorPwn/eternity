7b80bf9b290b8b15071b444a63186c996f7721aa
SkorPwn <kevin.jj.tran@gmail.com>
2015-09-14 16:40:21 +0200

    upload image gem carrier amazon

--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150914121608) do
+ActiveRecord::Schema.define(version: 20150914142400) do
 
   create_table "capsules", force: :cascade do |t|
     t.string   "message"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "image"
   end
 
 end
